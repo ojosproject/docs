013fb5284e0babae9e04c5fab5aced4e3fd0c240
Carlos Valdez <cvaldezh@uci.edu>
2024-03-21 16:34:18 -0700

    removed default comments from config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,18 +13,12 @@ const config: Config = {
 
   trailingSlash: true,
 
-  // Set the production url of your site here
   url: 'https://docs.ojosproject.org/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
@@ -72,7 +66,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/header.png',
     navbar: {
       title: 'Docs',
